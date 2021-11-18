634bb0534baecf3183891fa6a66d963788d656dd
Leandro <leandroquiroga9514@gmail.com>
2021-11-18 17:30:20 -0300

    models of database of users

---
+++ models/users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const schemaUser = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    company: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Cliente', schemaUser);
