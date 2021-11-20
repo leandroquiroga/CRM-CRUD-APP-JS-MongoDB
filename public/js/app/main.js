7d5011cc9f3cf94a39a4ace50a3332c7be1c72ab
Leandro <leandroquiroga9514@gmail.com>
2021-11-19 21:32:53 -0300

    Fixed a problem of cominication with DOM

--- public/js/app/main.js
+++ public/js/app/main.js
@@ -1,6 +1,6 @@
-const selector = (element) => document.querySelector(element);
-const selectorAll = (element) => document.querySelectorAll(element)
-const creator = (element) => document.querySelector(element);
+export const selector = (element) => document.querySelector(element);
+export const selectorAll = (element) => document.querySelectorAll(element)
+export const creator = (element) => document.querySelector(element);
 
 
 /* ========== Functions =================== */
@@ -11,17 +11,20 @@ const aside = () => {
     let addUser = selector('#new-users');
     let items = selectorAll('.items');
     let header = selector('.header');
-    let input = selector('.input')
+    let input = selector('.input');
+    let menu = selector('.menu-principal');
+    let card = selector('.card-form')
 
     if (aside.style.width == '300px') {
         header.classList.remove('justify-content-between', 'align-items-center')
         items[0].style.width = '70px'
         items[1].style.width = '70px'
         aside.style.width = '70px';
-        title.textContent = ''
+        title.textContent = '';
         user.classList.add('hidden');
         addUser.classList.add('hidden');
-        input.classList.add('hidden')
+        input.classList.add('hidden');
+        menu.style.width = "95%";
         return
     }
 
@@ -34,15 +37,11 @@ const aside = () => {
     items[0].style.width = '100%'
     items[1].style.width = '100%'
 
-}
-
-
+};
 const listeners = () => {
     let menu = selector('#menu');
     menu.addEventListener('click', aside)
-}
-
-
+};
 export const initAPP = () => {
     document.addEventListener('DOMContentLoaded', () => {
         listeners();
