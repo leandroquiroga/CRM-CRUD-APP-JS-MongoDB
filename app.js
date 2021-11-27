80388734b3b9475871e76a26cbb440cd73a0e765
Leandro <leandroquiroga9514@gmail.com>
2021-11-27 01:52:43 -0300

    Init server - Back-End

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const database = require('./database/db')
 const app = express();
 
 database;
+// middlewares
 app.use(express.json({
     type: "*/*"
 }));
@@ -13,11 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.use(morgan('tiny'))
 
-
+// routes
 app.get('/', (req, res) => {
     res.send('Servidor lenvado')
 })
 app.use('/clientes', users)
+
+//static
+app.use(express.static ('dist'))
 
 const port =  8080;
 app.listen(port , () => console.log(`Server running at: http://localhost:${port}`))
